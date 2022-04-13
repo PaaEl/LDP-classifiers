b6fb388c9c121ab0c4ff309b80334b899b4828ca
Daan Gast <daangast@gmail.com>
2022-04-13 10:52:15 +0200

    Added multiple dataset function to TestSuite

--- TestSuite.py
+++ TestSuite.py
@@ -5,15 +5,15 @@ from sklearn.model_selection import cross_validate
 import pandas as pd
 
 class TestSuite():
-    def __init__(self, database_name, epsilon_values=[0.1,1,2], classifiers=[LDPNaiveBayes(LDPid='LH')]):
-        self.database_name = database_name
+    def __init__(self, database_names, epsilon_values=[0.1,1,2], classifiers=[LDPNaiveBayes(LDPid='LH')]):
+        self.database_names = database_names
         self.epsilon_values = epsilon_values
         self.classifiers = classifiers
-        self.preprocessor = DataPreprocessor(database_name)
+        self.preprocessor = DataPreprocessor()
 
     
     def set_params(self, epsilon_values, classifiers, onehotencoded=False):
-        """ set_params: Sets the specific parameters that will need to be evaluated
+        """ Sets the specific parameters that will need to be evaluated
         Parameters
         ----------
         epsilon_values : {array} 
@@ -43,31 +43,32 @@ class TestSuite():
         None
         
         """
-        X, y = self.preprocessor.get_data(onehotencoded=self.onehotencoded)
-        allScoresDataFrame = pd.DataFrame()
-        for classifier in self.classifiers:
-            classifierDataFrame = pd.DataFrame()
-            for epsilon_value in self.epsilon_values:
-                classifier.set_params(epsilon=epsilon_value)
-                scores = cross_validate(classifier, X, y, scoring=['balanced_accuracy','f1_macro', 'precision_macro', 'recall_macro'])
-                scoresDataFrame = pd.DataFrame.from_dict(scores).mean()
-                scoresDataFrame.drop(labels=['fit_time', 'score_time'], inplace=True)
-                rowName = classifier.__str__() + '/' + self.database_name + '/'
-                scoresDataFrame = scoresDataFrame.add_prefix(rowName)
-                classifierDataFrame[epsilon_value] = scoresDataFrame
-            allScoresDataFrame = allScoresDataFrame.append(classifierDataFrame)
-        self.print_scores(allScoresDataFrame)
+        for database_name in self.database_names:
+            X, y = self.preprocessor.get_data(database_name, onehotencoded=self.onehotencoded)
+            allScoresDataFrame = pd.DataFrame()
+            for classifier in self.classifiers:
+                classifierDataFrame = pd.DataFrame()
+                for epsilon_value in self.epsilon_values:
+                    classifier.set_params(epsilon=epsilon_value)
+                    scores = cross_validate(classifier, X, y, scoring=['balanced_accuracy','f1_macro', 'precision_macro', 'recall_macro'])
+                    scoresDataFrame = pd.DataFrame.from_dict(scores).mean()
+                    scoresDataFrame.drop(labels=['fit_time', 'score_time'], inplace=True)
+                    rowName = classifier.__str__() + '/' + database_name + '/'
+                    scoresDataFrame = scoresDataFrame.add_prefix(rowName)
+                    classifierDataFrame[epsilon_value] = scoresDataFrame
+                allScoresDataFrame = allScoresDataFrame.append(classifierDataFrame)
+            self.print_scores(allScoresDataFrame)
 
     
     def print_scores(self, scores):
-        """ print_scores: Prints the scores in a specific format to a csv file
+        """ Prints the scores in a specific format to a csv file
         Parameters
         ----------
-        scores : {array}
+        scores : array, float
             Score values accuracy, f1, precision and recall
 
         Returns
         -------
         None
         """
-        scores.to_csv("./Experiments/test_run_" + date.today().__str__() + '.csv', sep=';', float_format='%.3f')
+        scores.to_csv("./Experiments/test_run_" + date.today().__str__() + '.csv', mode='a', sep=';', float_format='%.3f')
